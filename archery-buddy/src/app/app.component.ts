ae2b2ecbca8c133331f29799479d761b0bcedceb
niko7381 <niko.ringgaard@gmail.com>
2025-07-04 21:44:23 +0200

    changes to login site styles

--- archery-buddy/src/app/app.component.ts
+++ archery-buddy/src/app/app.component.ts
@@ -1,17 +1,12 @@
 import { Component } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { AuthService } from './services/Auth/auth.service';
-import { TrainingService } from './services/training/training.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-
   isAuthenticated = false;
-
   displayName: string;
 
   public appPages = [
@@ -21,7 +16,7 @@ export class AppComponent {
     { title: 'Physical-Training', url: '/physical-training-frontpage' },
     { title: 'Statistics', url: '/statistics-frontpage' },
     { title: 'Spec-Sheet', url: '/specsheet-frontpage' },
-    { title: 'Settings', url: '/settings' },
+    { title: 'Settings', url: '/settings' }
   ];
   constructor() {}
 }
